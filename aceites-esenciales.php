7c292c000aea5ee15646c0f883ed76409025318e
zurc <cruzjurado@gmail.com>
2014-04-09 08:58:03 +0100

    initial sketch

---
+++ aceites-esenciales.php
@@ -0,0 +1,219 @@
+<?php
+include("header.php"); //include header content
+?>
+
+
+	<div class="row wrap">
+
+	  <div class="large-12 columns">
+	  	<h2 class="centered-text light h2.smaller nmargin-top">Aceites Esenciales</h2>
+	  	<p class="centered-text lead">
+	  		Just Anticelulitis - Tonifica los contornos de tu cuerpo con hojas de vid roja y disfruta de una piel uniforme!
+		</p>
+	  	<div class="row">
+	  		<div class="large-6 columns">  		
+				<p>
+					Experimenta cuán armoniosamente coordinados estos productos brindan cuidado a tu piel y le ofrecen una nueva sensación. Así se ha desarrollado un innovador línea anti-celulitis para usted. Refinado y fortalecido en una nueva técnica científica contorno corporal - sobre la base de hierbas primitivos sólo se ha desarrollado un programa anti-celulitis única. Experiencia forma armoniosa productos de cuidado coordinado que dan una nueva sensación. Siente la suavidad con el uso regular, imperfecciones de la piel y su piel se ve más fresca y más firme.
+				</p>
+			</div>
+			<div class="large-6 columns centered-text">
+				<img src="img/anti-celulitis.jpg" alt="anti-celulitis">
+			</div>
+		</div>
+
+
+
+
+		<a name="lavanda-natural"></a>
+		<p class="npadding-top"></p>
+		<nav class="breadcrumbs mapa">
+		  <a href="index.html">Home</a>
+		  <a href="anti-celulitis.html">Anti-celulitis</a>
+		  <a class="current" href="anti-celulitis.html#lavanda-natural">Body Scrub</a>
+		</nav>
+		<h3 class="light">Anti-celulitis Body Scrub</h3>
+		<p class="lead">
+	  		El exfoliante corporal que estabas buscando!
+		</p>
+		<div class="row">
+			<div class="large-4 push-8 columns">
+				<div class="centered-text">
+					<img src="img/body-scrub.jpg" alt="body-scrub">
+				</div>
+			</div>
+			<div class="large-8 pull-4 columns">
+				<p>
+					La exfoliación es el paso básico y fundamental para el cuidado de la piel. Con suaves micro-gránulos de exfoliación en combinación con extractos de leche, miel, arroz y avena, remueven suavemente las células acumuladas en la superficie de la piel que obstaculizan la penetración de cualquier tratamiento que se coloque sobre ella. Deja la piel fresca y luminosa.
+				</p>
+				<p>
+					200ml - <strong>Cod:2000795</strong>
+				</p>
+			</div>
+		</div>
+
+
+
+
+
+
+		<a name="night-cream"></a>
+		<p class="npadding-top"></p>
+		<nav class="breadcrumbs mapa">
+		  <a href="index.html">Home</a>
+		  <a href="anti-celulitis.html">Anti-celulitis</a>
+		  <a class="current" href="anti-celulitis.html#night-cream">Night Cream</a>
+		</nav>
+
+		<h3  class="light">Anti-celulitis Night Cream</h3>
+		<p class="lead">
+	  		Fortalece su figura mientras duerme!
+		</p>
+		</p>
+		<div class="row">
+			<div class="large-4 columns">
+				<div class="centered-text">
+					<img src="img/night-cream.jpg" alt="body-scrub">
+				</div>
+			</div>
+			<div class="large-8 columns">
+				<p>
+					Experimenta el poder formativo de simplemente anti-celulitis crema de noche. Mientras usted duerme profundamente y relajado, la combinación especial de ingredientes activos desplegó su poder y apoya la capacidad de regeneración de la piel. Los extractos de hojas de vid roja apoyan la microcirculación, evitar la retención de agua antes y salen bultos desaparecen. Un complejo de habas y la rutina, la degradación prematura de la elastina y el colágeno. Esto promueve la restauración de la red de fibra de la piel, reduce las estrías y reduce al mínimo las manijas del amor. Disfrute de la fragancia delicada de su cuidado de la piel y una silueta corporal constante perfeccionamiento.
+					Probado por dermatólogos.
+				</p>
+				<p>
+					200ml - <strong>Cod:2000795</strong>
+				</p>
+			</div>
+		</div>
+		
+
+
+
+		<a name="firming-gel"></a>
+		<p class="npadding-top"></p>
+		<nav class="breadcrumbs mapa">
+		  <a href="index.html">Home</a>
+		  <a href="anti-celulitis.html">Anti-celulitis</a>
+		  <a class="current" href="anti-celulitis.html#firming-gel">Firming Gel</a>
+		</nav>
+
+		<h3  class="light">Anti-celulitis Firming Gel</h3>
+		<p class="lead">
+	  		Deléitese con su tono y silueta suave!
+		</p>
+		<div class="row">
+			<div class="large-4 push-8 columns">
+				<div class="centered-text">
+					<img src="img/firming-gel.jpg" alt="firming-gel">
+				</div>
+			</div>
+			<div class="large-8 pull-4 columns">
+				<p>
+					Diga adiós a los molestos baches e irregularidades de la piel. Sólo el gel tonificante refrescante con extractos de plantas de hojas de vid roja y sustancias altamente eficaces combate la celulitis tenaz repetidamente: Vid roja apoya la microcirculación y por lo tanto evita la retención de agua. Glaucina de alta calidad estimula la descomposición de la grasa en las células y contrarresta la formación de nuevas células de grasa. Pululano eficaz mejora la elasticidad y firmeza de la piel. Disfruta con sólo sentir la piel un tono parejo y, claramente contorneadas caderas y los muslos.
+				</p>
+				<p>
+					200ml - <strong>Cod:2000795</strong>
+				</p>
+			</div>
+		</div>
+
+
+
+		<a name="essential-care"></a>
+		<p class="npadding-top"></p>
+		<nav class="breadcrumbs mapa">
+		  <a href="index.html">Home</a>
+		  <a href="anti-celulitis.html">Anti-celulitis</a>
+		  <a class="current" href="anti-celulitis.html#essential-care">Essential care</a>
+		</nav>
+
+		<h3  class="light">Anti-celulitis Essential Care</h3>
+		<p class="lead">
+	  		Fortalece su figura mientras duerme!
+		</p>
+		</p>
+		<div class="row">
+			<div class="large-4 columns">
+				<div class="centered-text">
+					<img src="img/essential-care.jpg" alt="essential care">
+				</div>
+			</div>
+			<div class="large-8 columns">
+				<p>
+					Experimenta el poder formativo de simplemente anti-celulitis crema de noche. Mientras usted duerme profundamente y relajado, la combinación especial de ingredientes activos desplegó su poder y apoya la capacidad de regeneración de la piel. Los extractos de hojas de vid roja apoyan la microcirculación, evitar la retención de agua antes y salen bultos desaparecen. Un complejo de habas y la rutina, la degradación prematura de la elastina y el colágeno. Esto promueve la restauración de la red de fibra de la piel, reduce las estrías y reduce al mínimo las manijas del amor. Disfrute de la fragancia delicada de su cuidado de la piel y una silueta corporal constante perfeccionamiento.
+					Probado por dermatólogos.
+				</p>
+				<p>
+					200ml - <strong>Cod:2000795</strong>
+				</p>
+			</div>
+		</div>
+
+		
+
+	</div>
+
+	</div>
+</div>
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js"></script>
+  <script src="js/foundation.min.js"></script>
+  <script src="js/jquery.flexslider.js"></script>
+    <script type="text/javascript">
+    $(function(){
+      SyntaxHighlighter.all();
+    });
+    $(window).load(function(){
+      $('.flexslider').flexslider({
+        animation: "slide",
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+    });
+  </script>
+  <!--
+  
+  <script src="js/foundation/foundation.js"></script>
+  
+  <script src="js/foundation/foundation.alerts.js"></script>
+  
+  <script src="js/foundation/foundation.clearing.js"></script>
+  
+  <script src="js/foundation/foundation.cookie.js"></script>
+  
+  <script src="js/foundation/foundation.dropdown.js"></script>
+  
+  <script src="js/foundation/foundation.forms.js"></script>
+  
+  <script src="js/foundation/foundation.joyride.js"></script>
+  
+  <script src="js/foundation/foundation.magellan.js"></script>
+  
+  <script src="js/foundation/foundation.orbit.js"></script>
+  
+  <script src="js/foundation/foundation.reveal.js"></script>
+  
+  <script src="js/foundation/foundation.section.js"></script>
+  
+  <script src="js/foundation/foundation.tooltips.js"></script>
+  
+  <script src="js/foundation/foundation.topbar.js"></script>
+  
+  <script src="js/foundation/foundation.interchange.js"></script>
+  
+  <script src="js/foundation/foundation.placeholder.js"></script>
+  
+  <script src="js/foundation/foundation.abide.js"></script>
+  
+  -->
+  
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Just España</title>
+
+  <link href="http://fonts.googleapis.com/css?family=Quicksand:300,400,700|Oxygen:400,700,300" rel="stylesheet" type="text/css">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+	
+  <script src="js/vendor/custom.modernizr.js"></script>
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/general_foundicons.css">
+  <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+  
+</head>
+<body>
+
+<div class="container">
+	<div class="row wrap spadding">
+		<header>
+			<div class="small-6 large-4 columns headimg">
+				<a href="#">
+					<img src="img/head.png" alt="just">
+				</a>
+			</div>
+			<div class="small-6 large-5 columns right header-info">
+				<div class="header-contact">					
+					<p><i class="foundicon-phone style1"></i> 914 444 444</p>
+					<p><i class="foundicon-mail style1"></i> info@just-espana.es</p> 
+				</div>
+			</div>			
+		</header>
+	</div>
+
+<div class="row ">
+	<div class="contain-to-grid sticky">
+		<nav class="top-bar">
+
+			<div class="rectangle-left"></div>
+				<div class="arrow-left"></div>
+				  <ul class="title-area">
+				    <!-- Title Area -->
+				    <li class="name">
+				      <h1><a href="index.php">JUST ESPAÑA </a></h1>
+				    </li>
+				    <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+				    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+				  </ul>
+				<div class="rectangle-right"></div>
+			<div class="arrow-right"></div>
+
+		  <section class="top-bar-section">
+		    <!-- Left Nav Section -->
+		    <ul class="left">
+		      <li class="divider"></li>
+				<li class="has-dropdown"><a href="#">PRODUCTOS</a>
+			      <ul class="dropdown">
+			          <li class="has-dropdown"><a href="anti-celulitis.php">Anti-celulitis</a>
+			            <ul class="dropdown">
+			              <li><label>Anti-celulitis</label></li>
+			              <li><a href="anti-celulitis.php#body-scrub">Body scrub</a></li>
+			              <li><a href="anti-celulitis.php#night-cream">Night Cream</a></li>
+			              <li><a href="anti-celulitis.php#firming-gel">Firming gel</a></li>
+			              <li><a href="anti-celulitis.php#essential-care">Essential care</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="aceites-esenciales.php">Aceites Esenciales</a>
+						<ul class="dropdown">
+			              <li><label>Aceites Esenciales</label></li>
+			              <li><a href="aceites-esenciales.php#lavanda-natural">Lavanda Natural</a></li>
+			              <li><a href="aceites-esenciales.php#naranja-natural">Naranja Natural</a></li>
+			              <li><a href="aceites-esenciales.php#tea-tree">Tea Tree con Manuca Rosalina</a></li>
+			              <li><a href="aceites-esenciales.php#limon-natural">Limon Natural</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="aceites-esenciales.php">Baño y ducha</a>
+						<ul class="dropdown">
+			              <li><label>Baño y ducha</label></li>
+			              <li><a href="baño-ducha.php#lavanda-natural">Lavanda Natural</a></li>
+			              <li><a href="baño-ducha.php#naranja-natural">Naranja Natural</a></li>
+			              <li><a href="baño-ducha.php#tea-tree">Tea Tree con Manuca Rosalina</a>
+			              <li><a href="baño-ducha.php#limon-natural">Limon Natural</a></li>
+			              <li><a href="baño-ducha.php#lavanda-natural">Lavanda Natural</a></li>
+			              <li><a href="baño-ducha.php#naranja-natural">Naranja Natural</a></li>
+			              <li><a href="baño-ducha.php#tea-tree">Tea Tree con Manuca Rosalina</a></li>
+			              <li><a href="baño-ducha.php#limon-natural">Limon Natural</a></li>
+			              <li><a href="baño-ducha.php#tea-tree">Tea Tree con Manuca Rosalina</a>		              
+			            </ul>
+			          </li>
+			          <li class="divider"></li>
+			          <li class="has-dropdown"><a href="linea-edelweiss.php">línea Edelweiss</a>
+			            <ul class="dropdown">
+			              <li><label>linea-edelweiss</label></li>
+			              <li><a href="linea-edelweiss.php#1">1</a></li>
+			              <li><a href="linea-edelweiss.php#2">2</a></li>
+			              <li><a href="linea-edelweiss.php#3">3</a></li>
+			              <li><a href="linea-edelweiss.php#4">4</a></li>
+			            </ul>
+			          </li>
+			          <li class="divider"></li>
+			          <li class="has-dropdown"><a href="cuidado-de-pies.php">Cuidado de pies</a>
+			            <ul class="dropdown">
+			              <li><label>cuidado-de-pies</label></li>
+			              <li><a href="cuidado-de-pies.php#1">1</a></li>
+			              <li><a href="cuidado-de-pies.php#2">2</a></li>
+			              <li><a href="cuidado-de-pies.php#3">3</a></li>
+			              <li><a href="cuidado-de-pies.php#4">4</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="cuidado-facial.php">Cuidado Facial</a>
+			            <ul class="dropdown">
+			              <li><label>cuidado-facial</label></li>
+			              <li><a href="cuidado-facial.php#1">1</a></li>
+			              <li><a href="cuidado-facial.php#2">2</a></li>
+			              <li><a href="cuidado-facial.php#3">3</a></li>
+			              <li><a href="cuidado-facial.php#4">4</a></li>
+			              <li><a href="cuidado-facial.php#5">5</a></li>
+			              <li><a href="cuidado-facial.php#6">6</a></li>
+			              <li><a href="cuidado-facial.php#7">7</a></li>
+			              <li><a href="cuidado-facial.php#8">8</a></li>
+			              <li><a href="cuidado-facial.php#9">9</a></li>
+			              <li><a href="cuidado-facial.php#10">10</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="cuidado-del-cabello.php">Cuidado del Cabello</a>
+			            <ul class="dropdown">
+			              <li><label>cuidado-del-cabello</label></li>
+			              <li><a href="cuidado-del-cabello.php#1">1</a></li>
+			              <li><a href="cuidado-del-cabello.php#2">2</a></li>
+			              <li><a href="cuidado-del-cabello.php#3">3</a></li>
+			              <li><a href="cuidado-del-cabello.php#4">4</a></li>
+			              <li><a href="cuidado-del-cabello.php#5">5</a></li>
+			              <li><a href="cuidado-del-cabello.php#6">6</a></li>
+			              <li><a href="cuidado-del-cabello.php#7">7</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="productos-para-el-hogar.php">Productos para el Hogar</a>
+			            <ul class="dropdown">
+			              <li><label>productos-para-el-hogar</label></li>
+			              <li><a href="productos-para-el-hogar.php#1">1</a></li>
+			              <li><a href="productos-para-el-hogar.php#2">2</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="solo-para-hombres.php">Sólo para Hombres</a>
+			            <ul class="dropdown">
+			              <li><label>solo-para-hombres</label></li>
+			              <li><a href="solo-para-hombres.php#1">1</a></li>
+			              <li><a href="solo-para-hombres.php#2">2</a></li>
+			              <li><a href="solo-para-hombres.php#3">3</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="cuidado-del-cuerpo.php">Cuidado del Cuerpo</a>
+			            <ul class="dropdown">
+			              <li><label>cuidado-del-cuerpo</label></li>
+			              <li><a href="cuidado-del-cuerpo.php#1">1</a></li>
+			              <li><a href="cuidado-del-cuerpo.php#2">2</a></li>
+			              <li><a href="cuidado-del-cuerpo.php#3">3</a></li>
+			              <li><a href="cuidado-del-cuerpo.php#4">4</a></li>
+			              <li><a href="cuidado-del-cuerpo.php#5">5</a></li>
+			              <li><a href="cuidado-del-cuerpo.php#6">6</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="herboristeria.php">Herboristería</a>
+			            <ul class="dropdown">
+			              <li><label>herboristeria</label></li>
+			              <li><a href="herboristeria.php#1">1</a></li>
+			              <li><a href="herboristeria.php#2">2</a></li>
+			              <li><a href="herboristeria.php#3">3</a></li>
+			              <li><a href="herboristeria.php#4">4</a></li>
+			              <li><a href="herboristeria.php#5">5</a></li>
+			              <li><a href="herboristeria.php#6">6</a></li>
+			              <li><a href="herboristeria.php#7">7</a></li>
+			              <li><a href="herboristeria.php#8">8</a></li>
+			              <li><a href="herboristeria.php#9">9</a></li>
+			              <li><a href="herboristeria.php#10">10</a></li>
+			              <li><a href="herboristeria.php#11">11</a></li>
+			              <li><a href="herboristeria.php#12">12</a></li>
+			              <li><a href="herboristeria.php#13">13</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="linea-lamelloderm.php">Línea Lamelloderm</a>
+			            <ul class="dropdown">
+			              <li><label>linea-lamelloderm</label></li>
+			              <li><a href="linea-lamelloderm.php#1">1</a></li>
+			              <li><a href="linea-lamelloderm.php#2">2</a></li>
+			              <li><a href="linea-lamelloderm.php#3">3</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="linea-malven.php">Línea Malven</a>
+			            <ul class="dropdown">
+			              <li><label>linea-malven</label></li>
+			              <li><a href="linea-malven.php#1">1</a></li>
+			              <li><a href="linea-malven.php#2">2</a></li>
+			              <li><a href="linea-malven.php#3">3</a></li>
+			              <li><a href="linea-malven.php#4">4</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="nuevo.php">Nuevo</a>
+			            <ul class="dropdown">
+			              <li><label>nuevo</label></li>
+			              <li><a href="nuevo.php#1">1</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="cuidado-solar.php">Cuidado Solar</a>
+			            <ul class="dropdown">
+			              <li><label>cuidado-solar</label></li>
+			              <li><a href="cuidado-solar.php#1">1</a></li>
+			              <li><a href="cuidado-solar.php#2">2</a></li>
+			              <li><a href="cuidado-solar.php#3">3</a></li>
+			              <li><a href="cuidado-solar.php#4">4</a></li>
+			            </ul>
+			          </li>
+			        </ul>
+				</li>
+		      <li class="divider"></li>
+		      <li class="has-dropdown"><a href="#">ENCICLOPEDIA DE HIERBAS</a>
+			      <ul class="dropdown">
+			          <li class="has-dropdown"><a href="aceites.php">Aceites</a>
+			            <ul class="dropdown">
+			              <li><label>aceites</label></li>
+			              <li><a href="aceites.php#1">1</a></li>
+			              <li><a href="aceites.php#2">2</a></li>
+			              <li><a href="aceites.php#3">3</a></li>
+			              <li><a href="aceites.php#4">4</a></li>
+			              <li><a href="aceites.php#5">5</a></li>
+			              <li><a href="aceites.php#6">6</a></li>
+			              <li><a href="aceites.php#7">7</a></li>
+			              <li><a href="aceites.php#8">8</a></li>
+			              <li><a href="aceites.php#9">9</a></li>
+			              <li><a href="aceites.php#10">10</a></li>
+			              <li><a href="aceites.php#11">11</a></li>
+			              <li><a href="aceites.php#12">12</a></li>
+			              <li><a href="aceites.php#13">13</a></li>
+			              <li><a href="aceites.php#14">14</a></li>
+			              <li><a href="aceites.php#15">15</a></li>
+			              <li><a href="aceites.php#16">16</a></li>
+			              <li><a href="aceites.php#17">17</a></li>
+			              <li><a href="aceites.php#18">18</a></li>
+			              <li><a href="aceites.php#19">19</a></li>
+			              <li><a href="aceites.php#20">20</a></li>
+			              <li><a href="aceites.php#21">21</a></li>
+			              <li><a href="aceites.php#22">22</a></li>
+			              <li><a href="aceites.php#23">23</a></li>
+			              <li><a href="aceites.php#24">24</a></li>
+			              <li><a href="aceites.php#25">25</a></li>
+			              <li><a href="aceites.php#26">26</a></li>
+			            </ul>
+			          </li>
+			          <li class="has-dropdown"><a href="extractos.php">Extractos</a>
+			            <ul class="dropdown">
+			              <li><label>extractos</label></li>
+			              <li><a href="extractos.php#1">1</a></li>
+			              <li><a href="extractos.php#2">2</a></li>
+			              <li><a href="extractos.php#3">3</a></li>
+			              <li><a href="extractos.php#4">4</a></li>
+			              <li><a href="extractos.php#5">5</a></li>
+			              <li><a href="extractos.php#6">6</a></li>
+			              <li><a href="extractos.php#7">7</a></li>
+			              <li><a href="extractos.php#8">8</a></li>
+			              <li><a href="extractos.php#9">9</a></li>
+			              <li><a href="extractos.php#10">10</a></li>
+			              <li><a href="extractos.php#11">11</a></li>
+			              <li><a href="extractos.php#12">12</a></li>
+			              <li><a href="extractos.php#13">13</a></li>
+			              <li><a href="extractos.php#14">14</a></li>
+			              <li><a href="extractos.php#15">15</a></li>
+			              <li><a href="extractos.php#16">16</a></li>
+			              <li><a href="extractos.php#17">17</a></li>
+			              <li><a href="extractos.php#18">18</a></li>
+			              <li><a href="extractos.php#19">19</a></li>
+			              <li><a href="extractos.php#20">20</a></li>
+			              <li><a href="extractos.php#21">21</a></li>
+			              <li><a href="extractos.php#22">22</a></li>
+			              <li><a href="extractos.php#23">23</a></li>
+			              <li><a href="extractos.php#24">24</a></li>
+			            </ul>
+			          </li>
+			        </ul>
+				</li>
+		      <li class="divider"></li>
+		      <li class="has-dropdown"><a href="#">EMPRESA</a>
+
+		        <ul class="dropdown">
+		          <li><a href="#">Calidad Suiza</a></li>
+		          <li><a href="#">Historia</a></li>
+		          <li><a href="#">Just Suiza</a></li>
+		          <li><a href="#">Just Internacional</a></li>
+		          <li><a href="#">Equipo</a></li>
+		        </ul>
+		      </li>
+		      <li class="divider"></li>
+		    </ul>
+
+		    <!-- Right Nav Section -->
+		    <ul class="right">
+		      <li class="divider hide-for-small"></li>
+		      <li><a href="#">CONTACTO</a></li>
+		      <li class="divider"></li>
+		      <li class="has-form">
+		        <form>
+		          <div class="row collapse">
+		            <div class="small-8 columns">
+		              <input type="text">
+		            </div>
+		            <div class="small-4 columns">
+		              <a href="#" class="button">Busca</a>
+		            </div>
+		          </div>
+		        </form>
+		      </li>
+		    </ul>
+		  </section>
+		</nav>
+	</div>
+</div>
